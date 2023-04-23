f40f9e3b8c6de205243ac05dcb3e572691f6a2ec
Ojidaemo <vit.martsinovich@gmail.com>
2023-04-23 11:07:29 +0300

    Add words to categories, code cleaning

--- Croc Challenge/Model/Teams.swift
+++ Croc Challenge/Model/Teams.swift
@@ -11,12 +11,4 @@ struct Teams: Equatable {
     let name: String
     var score: Int
     let teamImage: UIImage
-    
-//    static func showTeam() -> [Teams] {
-//        var post = [Teams]()
-//        post.append(Teams(name: "Ковбои", score: 0, teamImage: UIImage(named: Resources.Image.cowboy)!))
-//        post.append(Teams(name: "Стройняшки", score: 0, teamImage: UIImage(named: Resources.Image.cowboy)!))
-//        post.append(Teams(name: "Красотки", score: 0, teamImage: UIImage(named: Resources.Image.cowboy)!))
-//        return post
-//    }
 }
